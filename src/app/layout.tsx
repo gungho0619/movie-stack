64d6e78f24aaca79902e8e23623190f16bc0e13d
gungho0619 <tzztntt@gmail.com>
2024-07-28 20:19:11 +0300

    font-fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const roboto = Roboto({ weight: ["500", "900"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["500", "900"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
